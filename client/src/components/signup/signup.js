e897785a4515d614ff1645d3745b45442a20bf5b
trngapp <74776793+trngapp@users.noreply.github.com>
2022-06-07 13:11:50 +0530

    checking

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Card from "@mui/material/Card"
 
 function Copyright(props) {
   return (
@@ -40,6 +41,7 @@ export default function SignUp() {
 
   return (
     <ThemeProvider theme={theme}>
+
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
