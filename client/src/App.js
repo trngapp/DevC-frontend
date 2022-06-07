d093b764b96a5b2f0587fe077dfd33f2fe49b42f
Harishita <harishitarana@gmail.com>
2022-06-07 21:25:27 +0530

    createus

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Admin from "./components/admin/admin"
 import Footer from "./components/footer/footer.js"
@@ -9,25 +9,28 @@ import Home from "./components/home/home"
 import Create from "./components/createform/create"
 import Signin from "./components/signin/signin"
 import Signup from "./components/signup/signup"
+import Createus from './components/createform/createus';
 
 const App = () => {
   return (
     <>
-    <div>
-      <Nav/>
-      <Routes>
+      <div>
+        <Nav />
+        <Routes>
 
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/create" element={<Create />} />
-<Route exact path="/admin" element={<Admin/>}/>
-<Route exact path="/sigin" element={<Signin/>}/>
-<Route exact path="/signup" element={<Signup/>}/>
-      </Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/create" element={<Create />} />
+          <Route exact path="/admin" element={<Admin />} />
+          <Route exact path="/sigin" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/form" element={<Createus />} />
 
-    </div>
+        </Routes>
 
-<Footer/>
-</>
+      </div>
+
+      <Footer />
+    </>
   );
 };
 
