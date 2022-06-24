172b30f065cec1fda3eb86fef7a6cc6ca0a5b6ff
Harishita <harishitarana@gmail.com>
2022-06-24 15:16:18 +0530

    Suggested initial changes done

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Create from "./components/createform/create"
 import Signin from "./components/signin/signin"
 import Signup from "./components/signup/signup"
 import Createus from './components/createform/createus';
+import FAQ from './components/faq/faq';
+import Aboutus from './components/aboutus/aboutus'
 
 const App = () => {
   return (
@@ -21,10 +23,12 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
          {/* <Route exact path="/create" element={<Create />} />*/}
           <Route exact path="/admin" element={<Admin />} />
-          <Route exact path="/sigin" element={<Signin />} />
+          <Route exact path="/signin" element={<Signin />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/create" element={<Createus />} />
           <Route exact path="/apply"  element={<Apply />} />
+          <Route exact path="/faq"   element={<FAQ />}/>
+          <Route exact path="/aboutus" element={<Aboutus />}/>
 
         </Routes>
 
