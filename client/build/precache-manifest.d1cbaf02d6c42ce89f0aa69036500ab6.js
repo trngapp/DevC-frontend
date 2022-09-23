d8dc1ea07a4efb882ace759547a799592bf364c7
Harishita <harishitarana@gmail.com>
2022-09-23 11:41:34 +0530

    build creation

---
+++ client/build/precache-manifest.d1cbaf02d6c42ce89f0aa69036500ab6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd2df1635ae80495da7",
+    "url": "/static/css/main.7e43dc8b.chunk.css"
+  },
+  {
+    "revision": "dbd2df1635ae80495da7",
+    "url": "/static/js/main.dbd2df16.chunk.js"
+  },
+  {
+    "revision": "42185f41813d223888a6",
+    "url": "/static/js/1.42185f41.chunk.js"
+  },
+  {
+    "revision": "cb5f3d3ddec8d0f95506",
+    "url": "/static/css/2.ada780ca.chunk.css"
+  },
+  {
+    "revision": "cb5f3d3ddec8d0f95506",
+    "url": "/static/js/2.cb5f3d3d.chunk.js"
+  },
+  {
+    "revision": "f59f0bd54de53600d64a",
+    "url": "/static/js/runtime~main.f59f0bd5.js"
+  },
+  {
+    "revision": "9df61ffe4c30b6f4267ffe4b3c518e7d",
+    "url": "/static/media/symbol.9df61ffe.png"
+  },
+  {
+    "revision": "5cd317629e44058fc67630b6ff8e1598",
+    "url": "/static/media/hello.5cd31762.jpg"
+  },
+  {
+    "revision": "cdfc5d6462ee2ce7ef5b33831de1e2c3",
+    "url": "/static/media/hii.cdfc5d64.jpg"
+  },
+  {
+    "revision": "daf559498b09cc36c57e7f8ac5538242",
+    "url": "/static/media/mobile.daf55949.png"
+  },
+  {
+    "revision": "f17add5067ee5f0e666dc281251afb13",
+    "url": "/static/media/entry.f17add50.png"
+  },
+  {
+    "revision": "b6542fb4da73e1505c689dd3d394747a",
+    "url": "/static/media/hello.b6542fb4.png"
+  },
+  {
+    "revision": "dd314d7c284e94e263a7a080415965e9",
+    "url": "/static/media/flow.dd314d7c.jpg"
+  },
+  {
+    "revision": "6ffd10385faa8a4bbc28dd660b11d074",
+    "url": "/static/media/heel.6ffd1038.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "08dab04381dda1a8d80444215b85dce7",
+    "url": "/index.html"
+  }
+];
