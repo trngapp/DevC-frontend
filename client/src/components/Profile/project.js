5f3f6ee7a1e118302260edf9acbac06f47a1f3ae
trngapp <74776793+trngapp@users.noreply.github.com>
2023-06-07 17:42:35 +0530

    added info in project page of profile

--- client/src/components/Profile/project.js
+++ client/src/components/Profile/project.js
@@ -19,9 +19,70 @@ const Project=()=>{
             color:"#C81132"
         },
     }));
+    const [logo,setlogo]=useState("");
+    const [position,setposition]=useState("");
+    const [email,setemail]=useState("");
+    const [name,setname]=useState("");
+    const [pname,setpname]=useState("");
+    const [workplace,setworkplace]=useState("");
+    const [desc,setdesc]=useState("");
+    const [type,settype]=useState("");
+    const [number,setnumber]=useState("");
+    const [expertise,setexp]=useState("");
+
     const [load,setLoad]=useState(false);
     useEffect(()=>{
+
         setLoad(true);
+        const value=localStorage.getItem("user");
+        axios.get(`https://main--polite-syrniki-ad57c8.netlify.app/.netlify/functions/api/projinfo?email=${value}`,{withCredentials:true}).then((res)=>{
+            console.log(res);
+            setposition(res.data[0].position);
+            setemail(res.data[0].leader_email);
+            setname(res.data[0].leader_name);
+            setpname(res.data[0].project_name);
+            setworkplace(res.data[0].workplace_type);
+            setdesc(res.data[0].project_desc);
+            settype(res.data[0].project_type);
+            setnumber(res.data[0].opening_number);
+            setexp(res.data[0].opening_expertise);
+
+
+
+            }).catch((error)=>{
+      console.log(error)
+
+            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         setTimeout(()=>{
           setLoad(false);
         },1000)
@@ -36,12 +97,14 @@ const Project=()=>{
                       <TextField
                           fullWidth
  label="First Name"
+ defaultValue={pname}
 />
                       </Grid>
                       <Grid item md={12} xs={12}>
                       <TextField
                           fullWidth
  label="First Name"
+ defaultValue={name}
 />
                           </Grid>
                           <Grid item xs={12} md={6}>
@@ -49,6 +112,7 @@ const Project=()=>{
 <TextField
     fullWidth
 label="First Name"
+defaultValue={position}
 />
 
 </Grid>
@@ -58,6 +122,7 @@ label="First Name"
 <TextField
 fullWidth
 label="First Name"
+defaultValue={workplace}
 />
 </Grid>
 <Grid item xs={12} md={6}>
@@ -65,6 +130,7 @@ label="First Name"
 <TextField
     fullWidth
 label="First Name"
+defaultValue={desc}
 />
 
 </Grid>
@@ -74,18 +140,21 @@ label="First Name"
 <TextField
 fullWidth
 label="First Name"
+defaultValue={type}
 />
 </Grid>
                           <Grid item md={12} xs={12}>
                           <TextField
                               fullWidth
  label="First Name"
+ defaultValue={number}
 />
                           </Grid>
                           <Grid item md={12} xs={12}>
                           <TextField
                               fullWidth
  label="First Name"
+ defaultValue={expertise}
 />
                           </Grid>
 
