87dbe713d6cf4b2aac530dda127a347a09b16809
trngapp <74776793+trngapp@users.noreply.github.com>
2023-07-12 12:50:36 +0530

    trying adding gmail checker, other changes in profile css and home css

--- client/src/components/signup/gmailCheck.js/gmailcheck.js
+++ client/src/components/signup/gmailCheck.js/gmailcheck.js
@@ -1,28 +1,27 @@
 import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Google from './gmail.js';
+
 //import ReactDOM from 'react-dom';
 //import GoogleLogin from 'react-google-login';
 // or
-import { GoogleLogin } from 'react-google-login';
 
 
-const responseGoogleSuccess = (response) => {
+
+/*const responseGoogleSuccess = (response) => {
   console.log(response);
 }
 const responseGoogleError = (response) => {
     console.log(response);
-  }
+  }*/
 
 
 const GmailCheck=()=>{
     return (
         <>
-<GoogleLogin
-    clientId="530815747234-dmje51rm1i1ktp4100gh360ba1e5ctom.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogleSuccess}
-    onFailure={responseGoogleError}
-    cookiePolicy={'single_host_origin'}
-  />
+<GoogleOAuthProvider clientId="530815747234-dmje51rm1i1ktp4100gh360ba1e5ctom.apps.googleusercontent.com">
+          <Google />
+        </GoogleOAuthProvider>
         </>
     )
 }
